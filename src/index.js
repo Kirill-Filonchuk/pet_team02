e12b19a3eaea24b2f6f147aee8e6dc48cfc40ad8
Anton <a.chertok@akatech.com.ua>
2023-03-17 22:03:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from 'router';
