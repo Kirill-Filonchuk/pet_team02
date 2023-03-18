8b1cf0b83533fc0a752afa0e3b93ca6c83d12cbe
Valeriia-Livinska <valerylivi@gmail.com>
2023-03-18 14:56:33 +0100

    fix normalize and fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from 'router';
 
