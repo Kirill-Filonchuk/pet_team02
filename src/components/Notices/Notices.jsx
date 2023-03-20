a018492b43a97b1d2f429a94a48677eaad6a3691
Anton <a.chertok@akatech.com.ua>
2023-03-20 12:10:34 +0200

    private routes in nav link

--- src/components/Notices/Notices.jsx
+++ src/components/Notices/Notices.jsx
@@ -12,19 +12,30 @@ import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { ROUTES } from 'router';
 import PageTitle from 'components/UIKit/PageTitle';
+import useAuth from 'hooks/useAuth/useAuth';
 // import SampleAddButton from 'components/AddButton/SampleAddButton';
 
 const Notices = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
+  //Temporary comented hook
+  // const { isLoggedIn } = useAuth();
+
+  const isLoggedIn = true;
+
   const noticesNavLinks = [
     { title: 'sell', to: ROUTES.NOTICES_SELL },
     { title: 'lost-found', to: ROUTES.NOTICES_LOST_FOUND },
     { title: 'in good hands', to: ROUTES.NOTICES_FOR_FREE },
-    { title: 'favorite ads', to: ROUTES.NOTICES_FAVORITE },
-    { title: 'my ads', to: ROUTES.NOTICES_OWN },
   ];
+
+  if (isLoggedIn) {
+    noticesNavLinks.push(
+      { title: 'favorite ads', to: ROUTES.NOTICES_FAVORITE },
+      { title: 'my ads', to: ROUTES.NOTICES_OWN }
+    );
+  }
 
   useEffect(() => {
     if (pathname === ROUTES.NOTICES) {
