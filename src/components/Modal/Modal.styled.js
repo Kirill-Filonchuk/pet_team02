73087618354bfda078116f15bee80dd4f7333919
Valeriia-Livinska <valerylivi@gmail.com>
2023-03-23 10:05:51 +0100

    fix reusable modal styles and add escbutton

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -5,24 +5,23 @@ export const Overlay = styled.div`
   z-index: 1200;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow-x: hidden;
-  overflow-y: hidden;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
   background-color: rgba(17, 17, 17, 0.6);
   backdrop-filter: blur(10px);
-
-  ${p => p.theme.mq.mobileOnly} {
-    padding: 160px 20px 20px 20px;
-  }
 `;
 
 export const ModalWrapper = styled.div`
-  position: relative;
+  position: absolute;
+  z-index: 1200;
+  top: 50%;
+  left: 50%;
+  overflow-y: auto;
+  border-radius: ${p => p.theme.radii.small};
   background-color: ${p => p.theme.colors.white};
-  border-radius: ${p => p.theme.radii.normal};
 
-  ${p => p.theme.mq.mobileOnly} {
-    border-radius: ${p => p.theme.radii.small};
+  ${p => p.theme.mq.tablet} {
+    border-radius: ${p => p.theme.radii.normal};
   }
 `;
