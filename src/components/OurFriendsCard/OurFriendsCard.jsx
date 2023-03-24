28254e4bbd894f01c75693ad371bada7ea547ad2
Volodymyr Husak <volodyaog2907@gmail.com>
2023-03-24 23:34:52 +0200

    add TitleContainer & ContactList

--- src/components/OurFriendsCard/OurFriendsCard.jsx
+++ src/components/OurFriendsCard/OurFriendsCard.jsx
@@ -1,11 +1,16 @@
 import propTypes from 'prop-types';
 import {
-  Title,
+  TitleContainer,
+  TitleLink,
   FriendsContainer,
   Image,
-  ContactContainer,
+  ContactList,
+  ContactItem,
   Link,
+  Address,
 } from './OurFriendsCard.styled';
+
+import defaultImage from 'assets/images/pets-default-image.jpg';
 
 const OurFriendsCard = ({
   address,
@@ -17,48 +22,77 @@ const OurFriendsCard = ({
   url,
   workDays,
 }) => {
-  const titleCounter = title => {
-    const lengthTitle = 23;
-    if (title.length > lengthTitle) {
-      const titleSlice = title.slice(0, lengthTitle);
-      return `${titleSlice}...`;
+  const dashDottedLine = '-----------------------------------';
+  // const titleCounter = title => {
+  //   const lengthTitle = 23;
+  //   if (title.length > lengthTitle) {
+  //     const titleSlice = title.slice(0, lengthTitle);
+  //     return `${titleSlice}...`;
+  //   }
+  //   return title;
+  // };
+  const contactCounter = contact => {
+    if (!contact) {
+      return dashDottedLine;
     }
-    return title;
+    const lengthContact = 24;
+    if (contact.length > lengthContact) {
+      const contactSlice = contact.slice(0, lengthContact);
+      return `${contactSlice}`;
+    }
+    return contact;
   };
   return (
     <>
-      <Title>{titleCounter(title)}</Title>
+      <TitleContainer>
+        <TitleLink href={url} target="_blank">
+          {title}
+        </TitleLink>
+      </TitleContainer>
       <FriendsContainer>
-        <Image alt={title} src={imageUrl} />
-        <ContactContainer>
-          <Link>
-            Time:
-            <br />
-            8:00- 20:00
-          </Link>
-          <Link href={addressUrl}>
-            Adress: <br />
-            {address}
-          </Link>
-          <Link>
-            Email: <br />
-            {email}
-          </Link>
-          <Link>
-            Phone: <br />
-            {phone}
-          </Link>
-        </ContactContainer>
+        <Image alt={title} src={imageUrl || defaultImage} />
+        <ContactList>
+          <ContactItem>
+            <Link>
+              Time:
+              <br />
+              8:00- 20:00
+            </Link>
+          </ContactItem>
+          <ContactItem>
+            <Link href={addressUrl || '#'} target={addressUrl && '_blank'}>
+              Adress: <br />
+              {address && <Address>{contactCounter(address)}</Address>}
+              {!address && dashDottedLine}
+            </Link>
+          </ContactItem>
+          <ContactItem>
+            <Link href={`mailto:${email || '#'}`}>
+              Email: <br />
+              {contactCounter(email)}
+            </Link>
+          </ContactItem>
+          <ContactItem>
+            <Link href={`tel::${phone || '#'}`}>
+              Phone: <br />
+              {contactCounter(phone)}
+            </Link>
+          </ContactItem>
+        </ContactList>
       </FriendsContainer>
     </>
   );
 };
 
 OurFriendsCard.propTypes = {
-  //   title: propTypes.string.isRequired,
-  //   url: propTypes.string.isRequired,
-  //   description: propTypes.string.isRequired,
-  //   date: propTypes.string,
+  address: propTypes.string,
+  addressUrl: propTypes.string,
+  email: propTypes.string,
+  imageUrl: propTypes.string,
+  phone: propTypes.string,
+  title: propTypes.string.isRequired,
+  url: propTypes.string,
+  workDays: propTypes.array,
 };
 
 export default OurFriendsCard;
