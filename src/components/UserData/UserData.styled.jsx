6f805d365735164ee1366c53c0a23b6a11c9ec33
AleksanrKotlyar <kotlyar.alek@gmail.com>
2023-03-24 12:51:24 +0200

    fix avatar sizeing

--- src/components/UserData/UserData.styled.jsx
+++ src/components/UserData/UserData.styled.jsx
@@ -108,7 +108,7 @@ export const CloseCircleLine = styled(RiCloseCircleLine)`
 export const StyledImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   border-radius: ${p => p.theme.radii.round};
 `;
 
