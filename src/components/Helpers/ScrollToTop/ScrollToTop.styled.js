340685c6227c6ca4ba9a799341f72fc781543534
Dima <57713733+Dima1409@users.noreply.github.com>
2023-03-26 22:49:24 +0300

    fix btn_to_top

--- src/components/Helpers/ScrollToTop/ScrollToTop.styled.js
+++ src/components/Helpers/ScrollToTop/ScrollToTop.styled.js
@@ -5,13 +5,11 @@ const ToTop = styled.div`
 position: relative;
 `
 const pulls = keyframes `
-from{
- transform:scale(1);
-    opacity:1;
+0% {
+    box-shadow: 0 0 0 0px rgba(0, 0, 0, 0.2);
   }
-  to{
-    opacity: 0.5;
-    transform:scale(1.2);
+  100% {
+    box-shadow: 0 0 0 20px rgba(0, 0, 0, 0);
   }
 `
 const Icon = styled.div`
@@ -19,13 +17,14 @@ const Icon = styled.div`
   bottom: 20px;
   right: 20px;
   border: 1px solid ${theme.colors.accent};
-  opacity: 0.5;
+  background-color: ${theme.colors.accent};
+  opacity: 0.7;
   border-radius: 50%;
   z-index: 20;
   width: 28px;
   height: 28px;
   transition: 0.3s;  
-  animation: ${pulls} 2s ease-out infinite normal;
+  animation: ${pulls} 3s ease-out infinite normal;
   ${theme.mq.tablet} {
     width: 40px;
     height: 40px;
@@ -33,7 +32,7 @@ const Icon = styled.div`
 
   & svg {
     position: absolute;
-    color: ${theme.colors.accent};
+    color: ${theme.colors.white};
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
@@ -43,8 +42,7 @@ const Icon = styled.div`
   &:hover {
   cursor: pointer;
   animation-play-state: paused;
-  opacity: 1;
-  box-shadow: 0 0 5px ${theme.colors.accent};
+  opacity: 1
   }
 `
 export {Icon, ToTop};
