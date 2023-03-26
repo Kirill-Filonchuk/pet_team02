33cbbf3e5a4ae2b93fffb176671cf79a9fc8192b
Oleksii26 <Kolesnik26@ukr.net>
2023-03-26 23:13:43 +0300

    heho fix finaly

--- src/pages/HomePage/HomePage.style.js
+++ src/pages/HomePage/HomePage.style.js
@@ -1,20 +1,19 @@
 import styled from 'styled-components';
-import DogMobx1 from '../../images/home/DogMobx1.png'
-import DogMobx2 from '../../images/home/DogMobx2.png'
+
 import DogDx1 from '../../images/home/DogDx1.png'
 import DogDx2 from '../../images/home/DogDx2.png'
-import DogTx1 from '../../images/home/DogTx1.png'
-import DogTx2 from '../../images/home/DogTx2.png'
-import {theme} from 'theme'
-import Wave  from '../../images/home/svg/Wave.svg'
-import WaveD  from '../../images/home/svg/WaveD.svg'
+import { theme } from 'theme'
+import Wave from '../../images/home/svg/Wave.svg'
+import WaveD from '../../images/home/svg/WaveD.svg'
+import Dogx1 from '../../images/home/Dogx1.png'
+import Dogx2 from '../../images/home/Dogx2.png'
 
 
 export const HeroTitle = styled.h1`
   font-size: 32px;
+  font-style: normal;
   font-weight: 700;
   line-height: 3, 14;
-  margin-top: 60px;
   margin-left: auto;
   margin-right: auto;
   width: 280px;
@@ -26,7 +25,6 @@ export const HeroTitle = styled.h1`
     line-height: 100px;
     width: 588px;
     height: 200px;
-    margin-top: 88px;
   margin-left: 33px;
 
     }
@@ -37,7 +35,6 @@ export const HeroTitle = styled.h1`
 font-weight: 700;
 font-size: 68px;
 line-height: 100px;
-margin-top: 92px;
   margin-left: 16px;
 
      }
@@ -45,40 +42,44 @@ margin-top: 92px;
 
 
 export const Hero = styled.div`
+padding-top: 60px;
 overflow: hidden;
 max-width: 767px;
-height: 500px;
-background-image: url('${DogMobx1}'), url(${Wave});
+height: 650px;
+background-image: url('${Dogx1}'), url(${Wave});
 background-repeat: no-repeat;
-background-position: 20% 180px, 20% 70px;
-background-size: 280px 524px, 620px 470px;
+background-position: center 174px, -250px 0;
+background-size: 280px 480px, 1000px 650px;
 
 @media (min-device-pixel-ratio: 2),
   (min-resolution: 192dpi),
   (min-resolution: 2dppx) {
-      background-image: url(${DogMobx2}), url(${Wave});
+      background-image: url(${Dogx2}), url(${Wave});
  
 }
  ${theme.mq.tablet} {
-  background-image: url('${DogTx1}') , url(${Wave});
+  padding-top: 88px;
+  background-image: url('${Dogx1}') , url(${Wave});
   max-width: 1279px;
-  height: 1100px;
+  height: 1400px;
   background-position: 50% 320px, 20% 0;
-  background-size: contain, 1449px 1190px;
+  background-size: 600px 1000px, 1449px 1190px;
 
   @media (min-device-pixel-ratio: 2),
   (min-resolution: 192dpi),
   (min-resolution: 2dppx) {
-      background-image: url(${DogTx2}), url(${Wave});
+      background-image: url(${Dogx2}), url(${Wave});
 }
  }
 
  ${theme.mq.desktop} {
+  position:absolute;
+  width: 100%; 
+  height: 100vh;
   max-width: 1800px;
-  height: 630px;
   background-image: url('${DogDx1}') , url(${WaveD});
-  background-position: right top, top;
-  background-size: 524px 918px, 1398px 770px;
+  background-position: bottom right;
+  background-size: 524px, contain;
     
   @media (min-device-pixel-ratio: 2),
   (min-resolution: 192dpi),
@@ -87,3 +88,5 @@ background-size: 280px 524px, 620px 470px;
 }
 
 `
+
+
