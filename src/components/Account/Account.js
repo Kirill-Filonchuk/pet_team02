95f03603e3d1b171fe8d85afeb4e1878f93b895d
Daria <100315552+DaryaHavrylenko@users.noreply.github.com>
2023-03-27 20:45:56 +0300

    fixed bugs from qa

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,13 +1,12 @@
-import { Button } from "./Account.styled"
-import { ReactComponent as AccountIcon } from '../../images/home/svg/account.svg'
-
+import { Button } from './Account.styled';
+import { ReactComponent as AccountIcon } from '../../images/home/svg/account.svg';
 
 const Account = () => {
-    return (
-        <Button to='/user'>
-            <AccountIcon style={{ marginRight: 12 }} /> Account
-        </Button>
-    )
-}
+  return (
+    <Button to="/user">
+      <AccountIcon style={{ marginRight: 12 }} /> Account
+    </Button>
+  );
+};
 
-export default Account
+export default Account;
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -8,6 +8,7 @@ export const Menu = styled.ul`
     align-items: center;
     width: 100%;
     text-decoration: none;
+    white-space: nowrap;
   }
   ${p => p.theme.mq.notDesktop} {
     display: none;
@@ -17,11 +18,21 @@ export const Wrapp = styled.li`
   ${p => p.theme.mq.desktop} {
     text-align: center;
     margin-right: 80px;
-    padding-top: 10px;
-    padding-bottom: 11px;
-    &:last-child {
-      margin-right: 0;
-    }
+    /* padding-top: 10px;
+    padding-bottom: 11px; */
+    font-display: swap;
+    font-family: ${p => p.theme.fonts.manrope};
+    font-size: ${p => p.theme.fontSizes[4]}px;
+    font-weight: ${p => p.theme.fontWeights.medium};
+    font-style: normal;
+    line-height: 27px;
+    letter-spacing: 0.04em;
+    text-decoration: none;
+    transition-property: color, transform;
+    transition-duration: ${p => p.theme.transitions.durations.default};
+  }
+  &:last-child {
+    margin-right: 0;
   }
 `;
 export const Link = styled(NavLink)`
@@ -29,12 +40,12 @@ export const Link = styled(NavLink)`
   text-align: center;
   text-decoration: none;
   color: black;
-  font-family: ${p => p.theme.fonts.manrope};
+  /* font-family: ${p => p.theme.fonts.manrope};
   font-size: ${p => p.theme.fontSizes[4]}px;
   font-weight: ${p => p.theme.fontWeights.medium};
   font-style: normal;
   line-height: 27px;
-  letter-spacing: 0.04em;
+  letter-spacing: 0.04em; */
   transition-property: color, transform;
   transition-duration: ${p => p.theme.transitions.durations.default};
 
