cd79774765fd18e323953c3d4d5ddbd4ef02fb9f
Kirill-Filonchuk <76174969+Kirill-Filonchuk@users.noreply.github.com>
2023-03-30 11:29:19 +0000

    Deploying to gh-pages from @ Kirill-Filonchuk/pet_team02@0cc5e583ef1a2a9d27e35f2c419e5a67d47eb341 🚀

--- static/js/373.d7eab993.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_team02=self.webpackChunkpet_team02||[]).push([[373],{4373:function(t,e,a){a.d(e,{QAE:function(){return u}});var n=a(9983);function u(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"}}]})(t)}}}]);
-//# sourceMappingURL=373.d7eab993.chunk.js.map
